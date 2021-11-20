d439037344b4ce27cb57adefeaeaaecedcfa3a73
luanmenezessilva <luanmenezessilva@outlook.com>
2021-11-20 18:19:48 -0300

    Criação do app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+// Método Get
+app.get('/', (request, response)=>{
+    return response.json({message: 'Servidor está funcionando'})
+})
+
+// Método Post
+app.post('/teste', (request, response)=>{
+    const {name,date} = request.body;
+    name = 'resposta' + name; // Concatenar para mostrar que deu certo
+
+    return response.json({name,date})
+})
+
+
+app.listen(3333)
